7b32df6711acf6f448a2e306fc9cd9cab915760b
Abhishek Shukla <120450523+TAbhishek-Shukla@users.noreply.github.com>
2022-12-29 00:50:01 -0800

    Update MenuItem.jsx

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,7 +5,7 @@ function MenuItem({ image, name, price ,key}) {
     <div className="menuItem" key={key}>
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
-      <p> ${price} </p>
+      <p>₹{price} </p>
     </div>
   );
 }
